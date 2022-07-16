6913885fc5b56c681a640fc15195a47c497656a6
jyun <you@example.com>
2022-07-16 02:28:52 -0400

    feat:movements ,  resume compoenent

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,29 @@
 <template>
     <Layout>
         <template #header>
-            <Header></Header>
+            <Header />
+        </template>
+        <template #resume>
+            <Resume />
+        </template>
+        <template #movements>
+            <Movements />
         </template>
     </Layout>
 </template>
 
 <script>
-import Layout from "./Layout.vue"
-import Header from "./Header.vue"
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume";
+import Movements from "./Movements.vue";
 
 export default {
     components: {
         Layout,
         Header,
+        Resume,
+        Movements,
     }
 }
 </script>
