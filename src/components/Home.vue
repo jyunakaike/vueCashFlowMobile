8ab926a9f3831f52232ebc5afd282f3c6589cc6c
jyun <you@example.com>
2022-07-17 13:22:44 -0400

    feat: resume component 1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,13 @@
             <Header />
         </template>
         <template #resume>
-            <Resume />
+            <Resume 
+                :total-label="'Ahorro total'"
+                :label="label"
+                :total-amount="1000000"
+                :amount="amount"
+            />
+
         </template>
         <template #movements>
             <Movements />
@@ -24,6 +30,12 @@ export default {
         Header,
         Resume,
         Movements,
+    },
+    data() {
+        return {
+            label: null,
+            amount: null, 
+        }
     }
 }
 </script>
