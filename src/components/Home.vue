436c0215411cad28cf32a2a7894e4ce63aa23c64
jyun <you@example.com>
2022-07-19 18:50:07 -0400

    fix: graphic tap, see last movents

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,7 @@
                 :amount="amount"
             >
                 <template #graphic>
-                    <Graphic :amounts="amounts" />
+                    <Graphic :amounts="amounts" @select="select" />
                 </template>
                 <template #action>
                     <Action @create="create" />
@@ -58,7 +58,7 @@ export default {
                 })
                 .map(m => m.amount)
             return lastDays.map((m, i) => {
-                const lastMovements = lastDays.slice(0, i);
+                const lastMovements = lastDays.slice(0, i+1);
                 return lastMovements.reduce((suma, movement) => {
                     return suma + movement
                 }, 0);
@@ -92,6 +92,10 @@ export default {
         },
         save() {
             localStorage.setItem("movements", JSON.stringify(this.movements));
+        },
+        select(el){
+            console.log(el);
+            this.amount = el;
         }
     }
 }
--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -1,34 +1,9 @@
 <template>
     <div>
-        <svg
-            @touchstart="tap"
-            @touchmove="tap"
-            @touchend="untap"
-            viewBox="0 0 300 200"
-        >
-            <line
-                stroke="#c4c4c4"
-                stroke-width="2"
-                x1="0"
-                :y1="zero"
-                x2="300"
-                :y2="zero"
-            />
-            <polyline
-                fill="none"
-                stroke="#0689B0"
-                stroke-width="2"
-                :points="points"
-            />
-            <line
-                v-show="showPointer"
-                stroke="#04b500"
-                stroke-width="2"
-                :x1="pointer"
-                y1="0"
-                :x2="pointer"
-                y2="200"
-            />
+        <svg @touchstart="tap" @touchmove="tap" @touchend="untap" viewBox="0 0 300 200">
+            <line stroke="#c4c4c4" stroke-width="2" x1="0" :y1="zero" x2="300" :y2="zero" />
+            <polyline fill="none" stroke="#0689B0" stroke-width="2" :points="points" />
+            <line v-show="showPointer" stroke="#04b500" stroke-width="2" :x1="pointer" y1="0" :x2="pointer" y2="200" />
         </svg>
         <p>Últimos 30 días</p>
         <!-- <div>{{ amounts }}</div> -->
@@ -36,7 +11,7 @@
 </template>
 
 <script setup>
-import { ref, toRefs, defineProps, defineEmits , computed } from 'vue';
+import { ref, toRefs, defineProps, defineEmits, computed, watch } from 'vue';
 const props = defineProps({
     amounts: {
         type: Array,
@@ -60,10 +35,17 @@ const points = computed(() => {
         const x = (300 / total) * (i + 1);
         const y = amountToPixels(amount);
         return `${points} ${x},${y}`;
-    }, "0, 100");
+    }, `0, ${amountToPixels(amounts.value.length ? amounts.value[0] : 0)}`);
 });
 const showPointer = ref(false);
 const pointer = ref(0);
+
+const emit = defineEmits(["select"]);
+watch(pointer, (value) => {
+    const index = Math.ceil((value / (300 / amounts.value.length)));
+    if (index < 0 || index > amounts.value.length) return;
+    emit("select", amounts.value[index - 1]);
+});
 
 const tap = ({ target, touches }) => {
     showPointer.value = true;
@@ -79,9 +61,10 @@ const untap = () => {
 
 <style scoped>
 svg {
-  width: 100%;
+    width: 100%;
 }
+
 p {
-  text-align: center;
+    text-align: center;
 }
 </style>
